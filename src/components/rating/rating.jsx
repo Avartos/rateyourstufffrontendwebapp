c43fb3fdc4ac322716e15bddde144144a0a26326
Avartos <59014772+Avartos@users.noreply.github.com>
2021-06-28 21:02:52 +0200

    some new shit

--- src/components/rating/rating.jsx
+++ src/components/rating/rating.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
+
 const rating = () => {
+
+    const NewRatinForm = ({handleSubmitForm}) =>{
+        const [title, setTitle] = useState("");
+        const [body, setBody] = useState("");
+        const [currentUser, setCurrentUser] = useState("");
+    
     return ( 
         <div>
-            
+            <div className="newRatingForm">
+                <form className="ratingBody"
+                    onSubmit={(e) => {
+                    handleSubmitForm(e, title, body, currentUser);
+                    setTitle("");
+                    setBody("");
+                    }}
+                >
+                    <label>Titel</label>
+                    <input
+                    type = "text"
+                    requiered
+                    value={title}
+                    onChange={(e) => {
+                        setTitle(e.target.value);
+                    }
+                    }
+                    />
+
+                </form>
+            </div>
         </div>
-     );
+        );
+    }
 }
  
 export default rating;
