d8680e8bce3ad35cec831d1947a25744efbeb3af
MKnop2 <kickmepony@gmail.com>
2021-06-28 20:54:57 +0200

    nothing works as intended needs to be fixed

--- src/components/headerBar.jsx
+++ src/components/headerBar.jsx
@@ -1,20 +1,41 @@
 import { Link } from "react-router-dom";
+import { ReactComponent as MenuIcon } from '../icons/menu.svg';
+
+import React, { useState, useEffect, useRef } from 'react';
+
+import SearchBar from './searchBar';
+import Togglebar from './togglebar';
+
 
 const HeaderBar = () => {
     return ( 
-        <nav>
-            <Link className="link" to="/">
-            <h2>RateYorStuff</h2>
-            </Link>
-            
+        <nav className="navbar">
+          <div className="leftside">
+            <NavItem icon={<MenuIcon />}>
+              <Togglebar></Togglebar>
+            </NavItem>
+            <h2>RateYourStuff</h2>
+          </div>
+          <SearchBar/>
             <p>
                 home
-                <div><Link className="link" to="/signup">SignUp</Link></div>
             </p>
-
-            <div className="gradient"></div>
         </nav>
      );
 }
- 
+
+function NavItem(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+
+      {open && props.children}
+    </li>
+  );
+}
+
 export default HeaderBar;
---
+++ src/components/togglebar.jsx
@@ -0,0 +1,93 @@
+import { ReactComponent as HomeIcon } from '../icons/home.svg';
+import { ReactComponent as NewsIcon } from '../icons/news.svg';
+import { ReactComponent as HistoryIcon } from '../icons/history.svg';
+import { ReactComponent as MyAccountIcon } from '../icons/myAccount.svg';
+import { ReactComponent as MyFriendsIcon } from '../icons/myFriends.svg';
+import { ReactComponent as MyRatingsIcon } from '../icons/myRatings.svg';
+import { ReactComponent as MyStuffIcon } from '../icons/myStuff.svg';
+import { ReactComponent as MoviesIcon } from '../icons/movies.svg';
+import { ReactComponent as SeriesIcon } from '../icons/series.svg';
+import { ReactComponent as BooksIcon } from '../icons/books.svg';
+import { ReactComponent as GamesIcon } from '../icons/games.svg';
+import { ReactComponent as OptionsIcon } from '../icons/options.svg';
+
+import { CSSTransition } from 'react-transition-group';
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
+
+function Togglebar(props) {
+    const [isVisible, setIsVisible] = useState();
+    
+    return (
+      
+      <CSSTransition 
+        in={isVisible}
+        classNames="leftNavs"
+        timeout={200}>
+        <div className="leftNavigation">
+          <div className="toggleLink">
+            <p className="icon-button">{<HomeIcon />}</p>
+            <Link to="/">Home</Link>
+          </div>
+          <div className="toggleLink">
+            <p className="icon-button">{<NewsIcon />}</p>
+            <Link to="/news">News</Link>
+          </div>
+          <div className="toggleLink">
+            <p className="icon-button">{<HistoryIcon />}</p>
+            <Link to="/history">History</Link>
+          </div>
+          < hr width="95%" align="center" size="1" ></hr>
+          <div className="toggleLink">
+            <p className="icon-button">{<MyAccountIcon />}</p>
+            <Link to="/myAccount">myAccount</Link>
+          </div>
+          <div className="toggleLink">
+            <p className="icon-button">{<MyFriendsIcon />}</p>
+            <Link to="/myFriends">myFriends</Link>
+          </div>
+          <div className="toggleLink">
+            <p className="icon-button">{<MyRatingsIcon />}</p>
+            <Link to="/myRatings">myRatings</Link>
+          </div>
+          <div className="toggleLink">
+            <p className="icon-button">{<MyStuffIcon />}</p>
+            <Link to="/myStuff">myStuff</Link>
+          </div>
+          < hr width="95%" align="center" size="1" ></hr>
+          <div className="toggleLink">
+            <p className="icon-button">{<MoviesIcon />}</p>
+            <Link to="/movies">Movies</Link>
+          </div>
+          <div className="toggleLink">
+            <p className="icon-button">{<SeriesIcon />}</p>
+            <Link to="/series">Series</Link>
+          </div>
+          <div className="toggleLink">
+            <p className="icon-button">{<BooksIcon />}</p>
+            <Link to="/books">Books</Link>
+          </div>
+          <div className="toggleLink">
+            <p className="icon-button">{<GamesIcon />}</p>
+            <Link to="/games">Games</Link>
+          </div>
+          < hr width="95%" align="center" size="1" ></hr>
+          <div className="toggleLink">
+            <p className="icon-button">{<OptionsIcon />}</p>
+            <Link to="/options">Options</Link>
+          </div>
+      </div>
+    </CSSTransition>
+    );
+  }
+
+  function TogglebarLinks(props) {
+    return (
+      <a href="#" className="menu-item" onClick={() => <Link to="/">Home</Link>}>
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+      </a>
+    );
+  }
+
+export default Togglebar
