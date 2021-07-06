71486c5ca09dce3d6b40412c650e91f17ce757a9
Chris <christoph.frischmuth@fh-erfurt.de>
2021-07-06 22:42:24 +0200

    # signUp.jsx -/ Validation for userName added

--- src/components/userManagement/signUp.jsx
+++ src/components/userManagement/signUp.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import RedirectingComponent from "./redirectingComponent";
 import {useHistory} from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 
 /**
@@ -14,7 +15,19 @@ const SignUp = () => {
     const [userName, setUserName] = useState('');
     const [passwordHash, setPassword] = useState('');
     const [email, setEmail] = useState('example@rays.com');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [status, setStatus] = useState(true)
     const _router = useHistory();
+
+    //TODO: Validation for email-address
+    function checkUserName() {
+        fetch(`http://localhost:5000/user/check/is=${userName}`, {
+            method: 'GET'
+        }).then((response) => {
+            setStatus(response.status === 418)
+        }).catch(error => {
+        })
+    }
 
     /**
      * Method generates the JSON-String after submit and sends fetches via POST-Request to the Rest-API
@@ -23,29 +36,35 @@ const SignUp = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         const isEnabled = false;
-        const login = { email, passwordHash, isEnabled }
+        const login = {email, passwordHash, isEnabled}
         const user = {firstName, gender, lastName, secondName, userName, login};
+        checkUserName();
+        if (status === false) {
+            fetch('http://localhost:5000/user/add', {
+                method: 'POST',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(user)
+            }).then(() => {
+                console.log('User successfully added');
+                setTimeout(() => _router.push('/login'), 1500);
+            })
+            console.log(user);
+        } else {
+            setErrorMessage("Username bereits vergeben!")
+            setTimeout(() => setErrorMessage(''), 3000);
+        }
 
-        fetch('http://localhost:5000/user/add', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json"},
-            body: JSON.stringify(user)
-        }).then(() => {
-            console.log('User successfully added');
-            setTimeout(() => _router.push('/login'), 1500);
-        })
-        console.log(user);
     }
     return (
         <div className="signUpDisplay">
             <div className="welcomeBox">
                 <h1>Welcome to ... </h1>
                 <p>This is your Plattform to rate all media you can imagine ...</p>
-                <p>You have the Opinion to share your progress of books you read, games you play, 
-                    series and movies you are actualy watching. The key to more fun is that you can 
-                    share that with your friends. Additionally you can discuss some chapters, 
-                    comment some progresses of your friends and many more! RateYourStuff can be 
-                    easily the key to Share all your favourite Media with your favourite People 
+                <p>You have the Opinion to share your progress of books you read, games you play,
+                    series and movies you are actualy watching. The key to more fun is that you can
+                    share that with your friends. Additionally you can discuss some chapters,
+                    comment some progresses of your friends and many more! RateYourStuff can be
+                    easily the key to Share all your favourite Media with your favourite People
                     and learn more about taste of them.</p>
                 <p>Just sign up and find out what media can do to you !!</p>
                 <p>Lets share our best moments with our friends.</p>
@@ -54,11 +73,11 @@ const SignUp = () => {
                 <h2>Account erstellen</h2>
                 <div className="signUp">
                     <form onSubmit={handleSubmit}>
-                        
+
                         <label>Gender</label>
                         <select className="genderChoiceField"
-                            value={gender}
-                            onChange={(e) => setGender(e.target.value)}
+                                value={gender}
+                                onChange={(e) => setGender(e.target.value)}
                         >
                             <option value="male">male</option>
                             <option value="female">female</option>
@@ -66,45 +85,46 @@ const SignUp = () => {
                         </select>
                         <label>First Name</label>
                         <input className="signUpInput"
-                            type="text"
-                            required
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
+                               type="text"
+                               required
+                               value={firstName}
+                               onChange={(e) => setFirstName(e.target.value)}
                         />
                         <label>Second Name</label>
                         <input className="signUpInput"
-                            type="text"
-                            value={secondName}
-                            onChange={(e) => setSecondName(e.target.value)}
+                               type="text"
+                               value={secondName}
+                               onChange={(e) => setSecondName(e.target.value)}
                         />
                         <label>Last Name</label>
                         <input className="signUpInput"
-                            type="text"
-                            required
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
+                               type="text"
+                               required
+                               value={lastName}
+                               onChange={(e) => setLastName(e.target.value)}
                         />
                         <label>User Name</label>
                         <input className="signUpInput"
-                            type="text"
-                            required
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
+                               type="text"
+                               required
+                               value={userName}
+                               onChange={(e) => setUserName(e.target.value)}
                         />
                         <label>Email</label>
                         <input className="signUpInput"
-                            title="email"
-                            required
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                               title="email"
+                               required
+                               value={email}
+                               onChange={(e) => setEmail(e.target.value)}
                         />
                         <label>Password</label>
                         <input className="signUpInput"
-                            type="password"
-                            required
-                            value={passwordHash}
-                            onChange={(e) => setPassword(e.target.value)}
+                               type="password"
+                               required
+                               value={passwordHash}
+                               onChange={(e) => setPassword(e.target.value)}
                         />
+                        {errorMessage}
                         <button className="signUpButton">SignUp</button>
                     </form>
                 </div>
