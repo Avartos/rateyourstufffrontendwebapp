08919546987e783ae5b25018c9a805d9cca2dd0c
Robin <robin.beck.we@gmail.com>
2021-08-12 14:38:25 +0200

    added edit function to movie details

---
+++ src/components/editMediaForms/editMovieForm.jsx
@@ -0,0 +1,239 @@
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router";
+import DefaultTextField from "../formComponents/defaultTextField";
+import DefaultAutoComplete from "../formComponents/defaultAutoComplete";
+import DefaultSelect from "../formComponents/defaultSelect";
+import AgeSelect from "../formComponents/ageSelect";
+import { Button } from "@material-ui/core";
+import ImagePreview from "../formComponents/imagePreview";
+import { useParams } from "react-router";
+
+const EditMovieForm = () => {
+  const [mediumName, setMediumName] = useState("");
+  const [releaseDate, setReleaseDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [duration, setDuration] = useState("");
+  const [ageRestriction, setAgeRestriction] = useState("");
+  const [mediumPoster, setMediumPoster] = useState("");
+  const [languages, setLanguages] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const history = useHistory();
+  const { id } = useParams();
+
+  const [genreList, setGenreList] = useState([]);
+  const [networkList, setNetworkList] = useState([]);
+  const [network, setNetwork] = useState(null);
+  const [languageList, setLanguageList] = useState([]);
+  const [currentImage, setCurrentImage] = useState("");
+  const [picturePath, setPicturePath] = useState('');
+
+  const handleGenreSelect = (e) => {
+    if (e.target.value.length <= 5) {
+      setGenres(e.target.value);
+    }
+  };
+
+  const fetchMovie = () => {
+    fetch(`http://localhost:5000/rest/movies/${id}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("unable to fetch movie");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setMediumName(data.mediumName);
+        setReleaseDate(data.releaseDate);
+        setCurrentImage(`http://localhost:5000/${data.picturePath}`);
+        setDescription(data.shortDescription);
+        setDuration(data.length);
+        setAgeRestriction(data.ageRestriction);
+        if(data.networkNetworkTitle)
+          setNetwork(data.networkNetworkTitle);
+        setLanguages(data.languages);
+        setGenres(data.genres);
+        setPicturePath(data.picturePath);
+      })
+      .catch((error) => {
+        history.push("/not_found");
+        console.error(error);
+      });
+  };
+
+  const fetchData = (targetUrl, title, setter) => {
+    fetch(targetUrl)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(`Unable to fetch ${title}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setter(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const handleLanguageSelect = (e) => {
+    if (e.target.value.length <= 15) {
+      setLanguages(e.target.value);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovie();
+    fetchData(
+      "http://localhost:5000/rest/languages/all",
+      "languages",
+      setLanguageList
+    );
+    fetchData("http://localhost:5000/rest/genres/all", "genres", setGenreList);
+    fetchData(
+      "http://localhost:5000/rest/networks/all",
+      "networks",
+      setNetworkList
+    );
+  }, []);
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    let movie = {
+      id: id,
+      mediumName: mediumName,
+      releaseDate: releaseDate,
+      shortDescription: description,
+      length: duration,
+      ageRestriction: ageRestriction,
+      languageStrings: languages,
+      genreStrings: genres,
+      networkTitle: network,
+    };
+
+    if(picturePath) {
+      movie.picturePath = picturePath
+    }
+
+    fetch("http://localhost:5000/rest/movies", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(movie),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error;
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        if (mediumPoster) {
+          const formData = new FormData();
+          formData.append("image", mediumPoster);
+          fetch(`http://localhost:5000/rest/movies/images/${data.id}`, {
+            method: "POST",
+            body: formData,
+          })
+            .then((response) => {
+              history.push(`/detail/movie/${data.id}`);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    console.log(network);
+  }, [network]);
+
+  const handleSelectImage = (event) => {
+    setCurrentImage(URL.createObjectURL(event.target.files[0]));
+  };
+
+  return (
+    <form className="addMediaForm" onSubmit={(e) => handleSubmitForm(e)}>
+      <span className="label">Poster</span>
+      <ImagePreview currentImage={currentImage} />
+      <input
+        type="file"
+        onChange={(e) => {
+          handleSelectImage(e);
+          setMediumPoster(e.target.files[0]);
+          console.log(e.target.files[0]);
+        }}
+      />
+
+      <DefaultTextField
+        title="Filmtitel"
+        value={mediumName}
+        setter={setMediumName}
+      />
+      <DefaultTextField
+        title="Veröffentlicht am"
+        value={releaseDate}
+        setter={setReleaseDate}
+        type="date"
+      />
+
+      <DefaultTextField
+        title="Kurzbeschreibung"
+        value={description}
+        setter={setDescription}
+        additionalOptions={{ multiline: true, rows: "10" }}
+      />
+
+      <DefaultTextField
+        title="Spieldauer (Minuten)"
+        value={duration}
+        setter={setDuration}
+        type="number"
+      />
+
+      <AgeSelect
+        title="Altersfreigabe"
+        value={ageRestriction}
+        setter={setAgeRestriction}
+      />
+
+      <DefaultAutoComplete
+        title="Netzwerk"
+        inputValues={networkList.map((network) => network.networkTitle)}
+        setter={setNetwork}
+        targetValue={network}
+      />
+
+      <DefaultSelect
+        title="Sprachen"
+        inputList={languageList.map((language) => {
+          return { id: languageList.id, title: language.language };
+        })}
+        targetValue={languages}
+        setter={handleLanguageSelect}
+      />
+      <DefaultSelect
+        title="Genres"
+        inputList={genreList.map((genre) => {
+          return { id: genre.id, title: genre.genreName };
+        })}
+        targetValue={genres}
+        setter={handleGenreSelect}
+        chipColor="secondary"
+        chipOutline="outlined"
+      />
+
+      <Button variant="contained" color="primary" type="submit">
+        Film hinzufügen
+      </Button>
+    </form>
+  );
+};
+
+export default EditMovieForm;
