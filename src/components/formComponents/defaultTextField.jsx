4954be444cd5ae7e083e3965732c94539ea23c35
Robin <robin.beck.we@gmail.com>
2021-08-12 10:22:35 +0200

    added add season form, fixed season display

--- src/components/formComponents/defaultTextField.jsx
+++ src/components/formComponents/defaultTextField.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
 
-const DefaultTextField = ({ title, value, setter, type = "default", additionalOptions, isRequired='true' }) => {
+const DefaultTextField = ({ title, value, setter, type = "default", additionalOptions, isRequired=true }) => {
   return (
     <React.Fragment>
       <span className="label">{title}{isRequired && '*'}</span>
       <TextField
         {...additionalOptions}
-        required
         value={value}
         type={type}
         required={isRequired}
