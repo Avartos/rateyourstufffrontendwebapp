2fd41af4cd7bfb97221809b8c609f75e422fbc1c
Chris <christoph.frischmuth@fh-erfurt.de>
2021-08-18 21:49:40 +0200

    # userPreview.jsx cleaned and finally commented # togglebar.jsx Authorizations

--- src/components/togglebar.jsx
+++ src/components/togglebar.jsx
@@ -1,140 +1,147 @@
-import { ReactComponent as HomeIcon } from "../icons/home.svg";
-import { ReactComponent as NewsIcon } from "../icons/news.svg";
-import { ReactComponent as HistoryIcon } from "../icons/history.svg";
-import { ReactComponent as MyAccountIcon } from "../icons/myAccount.svg";
-import { ReactComponent as MyFriendsIcon } from "../icons/myFriends.svg";
-import { ReactComponent as MyRatingsIcon } from "../icons/myRatings.svg";
-import { ReactComponent as MoviesIcon } from "../icons/movies.svg";
-import { ReactComponent as SeriesIcon } from "../icons/series.svg";
-import { ReactComponent as BooksIcon } from "../icons/books.svg";
-import { ReactComponent as GamesIcon } from "../icons/games.svg";
-import { ReactComponent as OptionsIcon } from "../icons/options.svg";
+import {ReactComponent as HomeIcon} from "../icons/home.svg";
+import {ReactComponent as NewsIcon} from "../icons/news.svg";
+import {ReactComponent as HistoryIcon} from "../icons/history.svg";
+import {ReactComponent as MyAccountIcon} from "../icons/myAccount.svg";
+import {ReactComponent as MyFriendsIcon} from "../icons/myFriends.svg";
+import {ReactComponent as MyRatingsIcon} from "../icons/myRatings.svg";
+import {ReactComponent as MoviesIcon} from "../icons/movies.svg";
+import {ReactComponent as SeriesIcon} from "../icons/series.svg";
+import {ReactComponent as BooksIcon} from "../icons/books.svg";
+import {ReactComponent as GamesIcon} from "../icons/games.svg";
+import {ReactComponent as OptionsIcon} from "../icons/options.svg";
 import CollectionsIcon from "@material-ui/icons/Collections";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
+import authorization from "../core/authorization";
 
-import { CSSTransition } from "react-transition-group";
-import React, { useState } from "react";
+import {CSSTransition} from "react-transition-group";
+import React, {useState} from "react";
 import ToggleLink from "./toggleLink";
 import helper from "../core/helper";
 
 function Togglebar(props) {
-  return (
-    <CSSTransition in={true} classNames="leftNavs" timeout={200}>
-      <div className="leftNavigation">
-        <ToggleLink
-          title="Home"
-          target="/"
-          handleSetVisible={props.handleSetVisible}
-        >
-          <HomeIcon />
-        </ToggleLink>
+    return (
+        <CSSTransition in={true} classNames="leftNavs" timeout={200}>
+            <div className="leftNavigation">
+                <ToggleLink
+                    title="Home"
+                    target="/"
+                    handleSetVisible={props.handleSetVisible}
+                >
+                    <HomeIcon/>
+                </ToggleLink>
 
-        <ToggleLink
-          title="News"
-          target="/news"
-          handleSetVisible={props.handleSetVisible}
-        >
-          <NewsIcon />
-        </ToggleLink>
+                <ToggleLink
+                    title="News"
+                    target="/news"
+                    handleSetVisible={props.handleSetVisible}
+                >
+                    <NewsIcon/>
+                </ToggleLink>
 
-        <ToggleLink
-          title="History"
-          target="/history"
-          handleSetVisible={props.handleSetVisible}
-        >
-          <HistoryIcon />
-        </ToggleLink>
-        <hr width="95%" align="center" size="1"></hr>
-        {helper.isLoggedIn() && (
-          <React.Fragment>
-            <ToggleLink
-              title="Mein Konto"
-              target="/myAccount"
-              handleSetVisible={props.handleSetVisible}
-            >
-              <MyAccountIcon />
-            </ToggleLink>
+                <ToggleLink
+                    title="History"
+                    target="/history"
+                    handleSetVisible={props.handleSetVisible}
+                >
+                    <HistoryIcon/>
+                </ToggleLink>
+                <hr width="95%" align="center" size="1"></hr>
+                {helper.isLoggedIn() && (
+                    <React.Fragment>
+                        <ToggleLink
+                            title="Mein Konto"
+                            target="/myAccount"
+                            handleSetVisible={props.handleSetVisible}
+                        >
+                            <MyAccountIcon/>
+                        </ToggleLink>
 
-            <ToggleLink
-              title="Freunde"
-              target="/myFriends"
-              handleSetVisible={props.handleSetVisible}
-            >
-              <MyFriendsIcon />
-            </ToggleLink>
+                        <ToggleLink
+                            title="Freunde"
+                            target="/myFriends"
+                            handleSetVisible={props.handleSetVisible}
+                        >
+                            <MyFriendsIcon/>
+                        </ToggleLink>
 
-            <ToggleLink
-              title="Bewertungen"
-              target="/myRatings"
-              handleSetVisible={props.handleSetVisible}
-            >
-              <MyRatingsIcon />
-            </ToggleLink>
+                        <ToggleLink
+                            title="Bewertungen"
+                            target="/myRatings"
+                            handleSetVisible={props.handleSetVisible}
+                        >
+                            <MyRatingsIcon/>
+                        </ToggleLink>
 
-            <ToggleLink
-              title="Meine Sammlungen"
-              target="/collections"
-              handleSetVisible={props.handleSetVisible}
-            >
-              <CollectionsIcon />
-            </ToggleLink>
+                        <ToggleLink
+                            title="Meine Sammlungen"
+                            target="/collections"
+                            handleSetVisible={props.handleSetVisible}
+                        >
+                            <CollectionsIcon/>
+                        </ToggleLink>
 
-            <hr width="95%" align="center" size="1"></hr>
-          </React.Fragment>
-        )}
+                        <hr width="95%" align="center" size="1"></hr>
+                    </React.Fragment>
+                )}
 
-        <ToggleLink
-          title="Filme"
-          target="/media/movies"
-          handleSetVisible={props.handleSetVisible}
-        >
-          <MoviesIcon />
-        </ToggleLink>
+                <ToggleLink
+                    title="Filme"
+                    target="/media/movies"
+                    handleSetVisible={props.handleSetVisible}
+                >
+                    <MoviesIcon/>
+                </ToggleLink>
 
-        <ToggleLink
-          title="Serien"
-          target="/media/series"
-          handleSetVisible={props.handleSetVisible}
-        >
-          <SeriesIcon />
-        </ToggleLink>
+                <ToggleLink
+                    title="Serien"
+                    target="/media/series"
+                    handleSetVisible={props.handleSetVisible}
+                >
+                    <SeriesIcon/>
+                </ToggleLink>
 
-        <ToggleLink
-          title="Bücher"
-          target="/media/books"
-          handleSetVisible={props.handleSetVisible}
-        >
-          <BooksIcon />
-        </ToggleLink>
+                <ToggleLink
+                    title="Bücher"
+                    target="/media/books"
+                    handleSetVisible={props.handleSetVisible}
+                >
+                    <BooksIcon/>
+                </ToggleLink>
 
-        <ToggleLink
-          title="Spiele"
-          target="/media/games"
-          handleSetVisible={props.handleSetVisible}
-        >
-          <GamesIcon />
-        </ToggleLink>
-        {helper.isLoggedIn() && (
-          <ToggleLink
-            title="Medium anlegen"
-            target="/add"
-            handleSetVisible={props.handleSetVisible}
-          >
-            <AddCircleIcon />
-          </ToggleLink>
-        )}
+                <ToggleLink
+                    title="Spiele"
+                    target="/media/games"
+                    handleSetVisible={props.handleSetVisible}
+                >
+                    <GamesIcon/>
+                </ToggleLink>
+                {helper.isLoggedIn() && (
+                    <ToggleLink
+                        title="Medium anlegen"
+                        target="/add"
+                        handleSetVisible={props.handleSetVisible}
+                    >
+                        <AddCircleIcon/>
+                    </ToggleLink>
+                )}
 
-        <hr width="95%" align="center" size="1"></hr>
-        <ToggleLink
-          title="Options"
-          target="/options"
-          handleSetVisible={props.handleSetVisible}
-        >
-          <OptionsIcon />
-        </ToggleLink>
-      </div>
-    </CSSTransition>
-  );
-}
+                <hr width="95%" align="center" size="1"></hr>
+                {authorization.isAdmin() && (
+                    <React.Fragment>
+                        <ToggleLink
+                            title="User Management"
+                            target="/adminpanel"
+                            handleSetVisible={props.handleSetVisible}
+                        >
 
-export default Togglebar;
+
+                    <OptionsIcon />
+                    </ToggleLink>
+                    </React.Fragment>
+                )}
+                    </div>
+                    </CSSTransition>
+                    );
+                    }
+
+                    export default Togglebar;
--- src/components/userManagement/userPreview.jsx
+++ src/components/userManagement/userPreview.jsx
@@ -2,28 +2,25 @@ import Switch from '../../components/switch';
 import {useState} from "react";
 import React from "react";
 
+const UserPreview = ({user}) => {
+
+
+
 /**
  * Component shows the User data of all users in database
- * also this component provides the possibility to delete users
- * IMPORTANT: Functions to disable users will follow on next stage of this project
+ * also
+ *   - this component provides the possibility to delete users
+ *   - change the state of the users, Enabled or Disabled
+ *   - change the role of the users; User, Admin, Moderator, or Business
  * @return {string}
  * @constructor
  * @param RoleId
  */
-
-
-
-const UserPreview = ({user}) => {
     /**
      * Function consumes the id of chosen user and is sending a delete request to backend
-     * @param id
+     * @param id, which refers to the id of the user to delete em
      */
-    const roles = ["User", "Admin", "Moderator", "Business"];
-    const {value, setValue} = useState('');
-    const {role, setRole} = useState('Admin');
     const {errorMessage, setErrorMessage} = useState('');
-    const roleId = user.roleId - 1;
-    console.log("User: " + user.userName + " Role: " + roles[roleId]);
 
     const handleDelete = (id) => {
         fetch(`http://localhost:5000/user/${user.id}`, {
@@ -32,18 +29,26 @@ const UserPreview = ({user}) => {
                 "Content-Type": "application/json",
                 "Authorization": sessionStorage.getItem("Bearer "),
             },
-        }).then(() => {
-            console.log('User successfully deleted');
+        }).then((response) => {
+            if(!response.ok){
+                throw Error("Löschen des Users fehlgeschlagen!")
+            }
+
         }).catch(error => {
-            console.log(error);
+            setErrorMessage(error.message);
         })
     }
 
 
+    /**
+     * Component handles the role update and is sending a put request to the backend api
+     * @param e even parameter
+     * @param userId, refers to the user which will be dated up
+     * @param roleId, refers to the chosen role
+     */
     const handleUserUpdate = (e, userId, roleId) => {
         e.preventDefault();
         const updatedUser = {id: userId, roleMappingId: roleId};
-        console.log(updatedUser);
         fetch('http://localhost:5000/user/customUpdate', {
             method: 'PUT',
             headers: {
@@ -56,16 +61,18 @@ const UserPreview = ({user}) => {
                 throw Error("Update des Profiles fehlgeschlagen!");
             }
         }).catch((error) => {
-            //setErrorMessage(error.message);
+            setErrorMessage(error.message);
         });
-        console.log(user);
 
     }
 
+    /**
+     * Component handles the En-/Disabling of the Users
+     * @param isEnabled, refers to the new state, Enabled or Disabled
+     */
     const handleEnabledSwitch = (isEnabled) => {
         const login = {isEnabled: isEnabled}
         const updatedUser = {id: user.id, login};
-        console.log(updatedUser);
         fetch('http://localhost:5000/user/isEnabledUpdate', {
             method: 'PUT',
             headers: {
@@ -75,17 +82,14 @@ const UserPreview = ({user}) => {
             body: JSON.stringify(updatedUser)
         }).then((response) => {
             if (!response.ok) {
-                throw Error("Update des Profiles fehlgeschlagen!");
+                throw Error("Update des von isEnabled fehlgeschlagen!");
             }
         }).catch((error) => {
-            //setErrorMessage(error.message);
+            setErrorMessage(error.message);
         });
-        console.log(user);
 
     };
 
-
-//TODO loginRoles (dropdown w. checkboxes...)n
 return (
     <div className="user-preview">
         <table>
@@ -99,13 +103,12 @@ return (
                 </th>
                 <th>
                     <select
-                        defaultValue={roles[user.roleId]}
                         onChange={(e) => handleUserUpdate(e, user.id, e.target.value)}
                     >
-                        <option value={1}>User</option>
-                        <option value={2}>Admin</option>
-                        <option value={3}>Moderator</option>
-                        <option value={4}>Business</option>
+                        <option value={1} selected={user.roleId === 1}>User</option>
+                        <option value={2} selected={user.roleId === 2}>Admin</option>
+                        <option value={3} selected={user.roleId === 3}>Moderator</option>
+                        <option value={4} selected={user.roleId === 4}>Business</option>
                     </select>
                 </th>
                 <th>
